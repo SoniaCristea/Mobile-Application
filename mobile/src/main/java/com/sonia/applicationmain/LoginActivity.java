e5d4fce29be90ba5a1048d6e93df33d94d041b30
SoniaCristea <soniacristea7@gmail.com>
2018-10-14 11:43:49 +0200

    Register link on main page with transition animation between activities

--- mobile/src/main/java/com/sonia/applicationmain/LoginActivity.java
+++ mobile/src/main/java/com/sonia/applicationmain/LoginActivity.java
@@ -1,17 +1,37 @@
 package com.sonia.applicationmain;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class LoginActivity extends AppCompatActivity{
+
+
+    private TextView registerLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        registerLink = findViewById(R.id.sign_up_link);
+
+
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+
+                LoginActivity.this.overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+
+            }
+        });
+
     }
 
-    public void login(){
-
-    }
 }
--- mobile/src/main/java/com/sonia/applicationmain/RegisterActivity.java
+++ mobile/src/main/java/com/sonia/applicationmain/RegisterActivity.java
@@ -1,12 +1,25 @@
 package com.sonia.applicationmain;
-
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-
 /**
  * Created by Sonia on 10/2/2018.
  */
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -14,8 +27,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+
+
     }
 
-    public void register(View view) {
-    }
 }
