4d63473b8d582db2abf83f392a34f4d4510366dd
rstruga <Razvan.Strugariu@ssi-schaefer.com>
2018-11-05 13:24:47 +0200

    fixed location finder

--- mobile/src/main/java/com/sonia/applicationmain/MapsActivity.java
+++ mobile/src/main/java/com/sonia/applicationmain/MapsActivity.java
@@ -1,22 +1,31 @@
 package com.sonia.applicationmain;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private LatLng currentLocation;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +35,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        currentLocation = getIntent().getExtras().getParcelable("location");
-        Log.i("loc",currentLocation.toString());
+//        currentLocation = getIntent().getExtras().getParcelable("location");
+//        Log.i("loc",currentLocation.toString());
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        mMap.addMarker(new MarkerOptions().position(currentLocation).title("Here is your car"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
 
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,15));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        }
+
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                //Log.w("INFO ", location.toString());
+                if (location != null) {
+                    Log.w("INFO ", location.getLongitude() + "   " + location.getLatitude());
+                    mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(location.getLatitude(), location.getLongitude()))
+                            .title("HERE IS MY CAR").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 20.0f));
+                }
+                else{
+                    Log.w("INFO ", "no location found");
+                }
+
+            }
+        });
+
+
+//        mMap.addMarker(new MarkerOptions().position(currentLocation).title("Here is your car"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+//
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,15));
     }
 
 }
